b2e7fc749832032e8ddc3fb8f04b724c4b2b5264
Nelson Rivera <riveranelson99@gmail.com>
2021-11-02 16:51:07 -0500

    Added necessary information to category route file

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -7,7 +7,12 @@ router.get('/', (req, res) => {
   try {
     // find all categories
     // be sure to include its associated Products
+    const categoryData = await Category.findAll({
+      include: [{ model: Product}],
+    });
+    res.status(200).json(categoryData);
   } catch (err) {
+    res.status(500).json(err);
   }
 });
 
@@ -15,29 +20,70 @@ router.get('/:id', (req, res) => {
   try {
     // find one category by its `id` value
     // be sure to include its associated Products
+    const categoryData = await Category.findByPk(req.params.id, {
+      include: [{ model: Product}],
+    });
+
+    if (!categoryData) {
+      res.status(404).json({ message: 'No category found with that id!' });
+      return;
+    }
+
+    res.status(200).json(categoryData);
   } catch (err) {
+    res.status(500).json(err);
   }
 });
 
 router.post('/', (req, res) => {
   try {
     // create a new category
+    const newCategory = await Category.create({
+      category_name: req.body.category_name,
+    });
+    res.status(200).json(newCategory);
   } catch (err) {
+    res.status(500).json(err);
   }
 });
 
 router.put('/:id', (req, res) => {
   try {
     // update a category by its `id` value
+    const categoryData = await Category.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!categoryData) {
+      res.status(404).json({ message: 'No category found with that id!' });
+      return;
+    }
+
+    res.status(200).json(categoryData);
   } catch (err) {
+    res.status(500).json(err);
   }
 });
 
 router.delete('/:id', (req, res) => {
   try {
     // delete a category by its `id` value
+    const categoryData = await Category.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!categoryData) {
+      res.status(404).json({ message: 'No category found with that id!' });
+      return;
+    }
+
+    res.status(200).json(categoryData);
   } catch (err) {
-    
+    res.status(500).json(err);
   }
 });
 

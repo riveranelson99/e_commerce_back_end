f42036980818beb10d2a6d2f8ffda4a912afaf94
Nelson Rivera <riveranelson99@gmail.com>
2021-11-03 14:29:37 -0500

    Small changes to file structure

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -40,18 +40,21 @@ router.get('/:id', async (req, res) => {
 // create new product
 router.post('/', (req, res) => {
   // try{
-  //   const newProduct = Product.create({
-  //     product_name: req.body.product_name,
-  //     price: req.body.price,
-  //     stock: req.body.stock,
-  //     tagIds: req.body.tagIds,
-  //   });
-    
+  //   const newProduct = await Product.create(req.body);
+
+  //   if (req.body.tagIds.length) {
+  //     const productTagIdArr = req.body.tagIds.map((tag_id) => {
+  //       return {
+  //         product_id: product.id,
+  //         tag_id,
+  //       };
+  //     });
+  //     return ProductTag.bulkCreate(productTagIdArr);
+  //   }    
   //   res.status(200).json(newProduct);
   // } catch (err) {
   //   res.status(500).json(err);
   // }
-
 
   /* req.body should look like this...
     {
@@ -85,6 +88,38 @@ router.post('/', (req, res) => {
 
 // update product
 router.put('/:id', (req, res) => {
+  // try {
+  //   await Product.update(req.body, {
+  //     where: {
+  //       id: req.params.id,
+  //     },
+  //   });
+
+  //   const productTags = await ProductTag.findAll({ where: { product_id: req.params.id }});
+  //   const productTagIds = productTags.map(({ tag_id }) => tag_id);
+  //   const newProductTags = req.body.tagIds
+  //     .filter((tag_id) => !productTagIds.includes(tag_id))
+  //     .map((tag_id) => {
+  //       return {
+  //         product_id: req.params.id,
+  //         tag_id,
+  //       };
+  //     });
+
+  //   const productTagsToRemove = productTags
+  //   .filter(({ tag_id }) => !req.body.tagIds.includes(tag_id))
+  //   .map(({ id }) => id);
+
+  //   const updatedProductTags = await Promise.all([
+  //     ProductTag.destroy({ where: { id: productTagsToRemove } }),
+  //     ProductTag.bulkCreate(newProductTags),
+  //   ]);
+
+  //   res.status(200).json(updatedProductTags);
+  // } catch (err) {
+  //   res.status(500).json(err);
+  // }
+
   // update product data
   Product.update(req.body, {
     where: {
